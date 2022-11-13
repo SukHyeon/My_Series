9901704e5a6eef4a9de6f2249af9a8a8421bcf13
HamSik <109560875+SukHyeon@users.noreply.github.com>
2022-11-13 20:11:37 +0900

    first commit

---
+++ My_Diary/My_Diary/My_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_DiaryApp.swift
+//  My_Diary
+//
+//  Created by 황석현 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct My_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
